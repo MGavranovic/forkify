11f864951b7fb84d6f3d848014571baadcead376
MGavranovic <mgavranovic7@gmail.com>
2022-11-04 22:15:17 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '956f2ed8-c030-44a0-aa17-2b7e1e7240db';
+export const MODAL_CLOSE_SEC = 2.5;
